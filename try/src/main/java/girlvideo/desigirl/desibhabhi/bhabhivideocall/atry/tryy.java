ece92ee3b37741b740428c4df53122784ecb0d37
Udit1049 <uditpatel194@gmail.com>
2021-11-10 14:32:07 +0530

    Initial commit

---
+++ try/src/main/java/girlvideo/desigirl/desibhabhi/bhabhivideocall/atry/tryy.java
@@ -0,0 +1,11 @@
+package girlvideo.desigirl.desibhabhi.bhabhivideocall.atry;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class tryy {
+
+    public static  void Simpletost(Context context, String Message){
+        Toast.makeText(context, Message, Toast.LENGTH_SHORT).show();
+    }
+}
